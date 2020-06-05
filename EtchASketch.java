affef25fe04d0d8ec061158da9aeee5f2775cd5f
GurenMkII <62103374+GurenMkII@users.noreply.github.com>
2020-06-05 10:52:01 -0400

    Add color options

--- EtchASketch.java
+++ EtchASketch.java
@@ -49,6 +49,16 @@ public class EtchASketch extends Canvas
 				x -= 10;
 			if ( e.getKeyCode() == KeyEvent.VK_RIGHT )
 				x += 10;
+			if ( e.getKeyCode() == KeyEvent.VK_F1 ) //Change color to red
+                cur = Color.red;
+            if ( e.getKeyCode() == KeyEvent.VK_F2 ) //Change color to green
+                cur = Color.green;
+            if ( e.getKeyCode() == KeyEvent.VK_F3 ) //Change color to blue
+                cur = Color.blue;
+            if ( e.getKeyCode() == KeyEvent.VK_F5 ) //Change color to black
+                cur = Color.black;
+            if ( e.getKeyCode() == KeyEvent.VK_F4 ) //Change color to black
+                cur = Color.white;
 
 			// and we manually call paint() again to redraw
 			repaint();
